9b2dec3d888a963c54d99c345b275e26fedd7e70
Yu Li <YLiLarry@gmail.com>
2016-11-19 00:00:53 -0500

    add discovery

--- app/components/home-search-results.js
+++ app/components/home-search-results.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-   looking: true,
+   looking: false,
    found: 0,
    message: Ember.computed('looking', 'found', function() {
       if (this.get('looking')) {
          return 'Computing all possible schedules for you';
       }
+      if (this.get('plans').length == 0) {
+         return "Couldn't find a schedule. You will have time conflicts taking these courses.";
+      }
+      return "Here's a list of schedules you can choose.";
    }),
    plans: R.map((id)=>({id:id}), R.range(1,10))
 });
